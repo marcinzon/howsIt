51cbe4190395ea3bce657c7f1fc3a3b4b6664dfc
Marc Inzon <marcinzon@gmail.com>
2017-10-03 13:50:54 +1300

    Minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-var surveyResults = [];
+$(".reveal").click(function() {
+
+	$(this).parent().next().removeClass('hidden');
+});
+
+$(".redo").click(function() {
+	$('#results').addClass('hidden');
+    $('html, body').animate({
+        scrollTop: $("#survey").offset().top
+    }, 2000);
+});
