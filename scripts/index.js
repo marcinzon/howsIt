ba610f6b1b5f2e6535be670b596325597b338f7d
Marc Inzon <marcinzon@gmail.com>
2017-10-03 17:37:10 +1300

    80% functional

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,55 @@
-$(".reveal").click(function() {
+$(document).ready( function(){
 
-	$(this).parent().next().removeClass('hidden');
+console.log("ready!");
+
+var answerArray= [];
+var tempAnswer;
+
+$('.answerBox').click(function(event) {
+
+	$(this).parents().next('.qNext').removeClass('hidden');
+	
+	if($(this).siblings().hasClass('selected')){
+		$(this).siblings().removeClass('selected');
+		$(this).addClass('selected');
+	}
+	else{
+		$(this).addClass('selected');
+	}
+
+		tempAnswer = event.target.id;
+		$('#temp').remove();
+		$('.testing').append("<span id="+"temp"+"><p>tempAnswer = "+tempAnswer+"</p></span>");
+
 });
+
+// SCROLL ANIMATION
+$('.qNext').click(function() {
+
+		answerArray.push(tempAnswer);
+
+		$('#tempArr').remove();
+		$('.testing').append("<span id="+"tempArr"+"><p>answerArray contents = "+answerArray+"</p></span>");
+
+		$(this).parent().next().removeClass('hidden');
+	    $('html, body').animate({
+        scrollTop: $(this).parent().next().offset().top
+    }, 500);
+});
+
+
 
 $(".redo").click(function() {
 	$('#results').addClass('hidden');
     $('html, body').animate({
         scrollTop: $("#survey").offset().top
     }, 2000);
+});
+
+function scrollReveal(){
+
+}
+
+
+
 });
